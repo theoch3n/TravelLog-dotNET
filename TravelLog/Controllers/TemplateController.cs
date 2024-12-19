ef7d67f0a7054a8b0faab34f292d5dc566898f08
JEC <wiseinfo31@gmail.com>
2024-12-19 17:04:02 +0800

    [fix] 1.Index.cshtml刪除Layout註解 2.修復Index2.cshtml

---
+++ TravelLog/Controllers/TemplateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelLog.Controllers
+{
+    public class TemplateController : Controller
+    {
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
