a48d059f1c5e170d24cdb84659fa19bf9c2d4341
theoch3n <theoch3n@gmail.com>
2024-12-20 16:37:14 +0800

    test(圖表): 1. 使用 chart.js 加入長條圖測試 (未連接資料庫)

---
+++ TravelLog/Controllers/ChartController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelLog.Controllers {
+  public class ChartController : Controller {
+    public IActionResult Index() {
+      return View();
+    }
+  }
+}
---
+++ TravelLog/wwwroot/js/chart.js
@@ -0,0 +1,20 @@
+﻿const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  datasets: [{
+    label: '2024 年每月遊客數',
+  data: [109, 98, 77, 136, 201, 156, 311, 114, 165, 175, 89, 211],
+  borderWidth: 1
+      }]
+    },
+  options: {
+    scales: {
+    y: {
+    beginAtZero: true
+        }
+      }
+    }
+  });
