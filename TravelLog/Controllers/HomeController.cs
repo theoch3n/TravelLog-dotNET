fbbf626c8519605786c4913eadc34e6e52a96086
theoch3n <theoch3n@gmail.com>
2024-12-26 15:19:10 +0800

    [chore] 1. 連接TravelLog資料庫 (EF Core Power Tools)
    [feat](訂單管理)
    1. 加入OrderController
    2. 加入OrderManage檢視頁面

--- TravelLog/Controllers/HomeController.cs
+++ TravelLog/Controllers/HomeController.cs
@@ -2,31 +2,33 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TravelLog.Models;
 
-namespace TravelLog.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace TravelLog.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        TravelLogContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger, TravelLogContext context) {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult OrderManage() {
+            var orders = _context.Orders.ToList(); // 確保這裡獲取的資料不為 null
+            return View(orders);
+        }
+
     }
 }
--- TravelLog/Program.cs
+++ TravelLog/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TravelLog.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,11 +8,14 @@ builder.Services.AddControllersWithViews();
 //µù¥USession
 builder.Services.AddSession();
 
+// Add services to the DI container.
+builder.Services.AddDbContext<TravelLogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TravelLog")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
