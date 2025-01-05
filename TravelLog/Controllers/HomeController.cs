94d46a8abe598a2ff9102ebc29441dc135e78be1
Ji En <Ji En@DESKTOP-VK5815U>
2025-01-05 15:59:19 +0800

    [feat] 修正取流水號異常問題

--- TravelLog/Controllers/HomeController.cs
+++ TravelLog/Controllers/HomeController.cs
@@ -1,29 +1,61 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
 using TravelLog.Models;
 
-namespace TravelLog.Controllers {
-    public class HomeController : Controller {
+
+namespace TravelLog.Controllers
+{
+    public class HomeController : Controller
+    {
         private readonly ILogger<HomeController> _logger;
         TravelLogContext _context;
 
-        public HomeController(ILogger<HomeController> logger, TravelLogContext context) {
+        public HomeController(ILogger<HomeController> logger, TravelLogContext context)
+        {
             _logger = logger;
             _context = context;
         }
+        //public IActionResult Index()
+        //{
+        //    SerialBase_SP serialBaseSP = new SerialBase_SP("UR");
+        //    DataSet ds = serialBaseSP.get_SerialNumber();
+        //    DataTable dt = ds.Tables[0];
+        //    SerialBase serialBase = null;
+        //    if (dt.Rows.Count > 0)
+        //    {
+        //        DataRow row = dt.Rows[0];
+        //        serialBase = new SerialBase
+        //        {
+        //            SbSerial = row["SB_Serial"] != DBNull.Value ? Int32.Parse(row["SB_Serial"].ToString()) : 0,
+        //            SbSystemCode = row["SB_SystemCode"]?.ToString(),
+        //            SbSystemName = row["SB_SystemName"]?.ToString(),
+        //            SbSerialNumber = row["SB_SerialNumber"]?.ToString(),
+        //            SbCount = row["SB_Count"] != DBNull.Value ? Int32.Parse(row["SB_Count"].ToString()) : 0,
+        //            ModifiedDate = row["ModifiedDate"] != DBNull.Value ? DateTime.Parse(row["ModifiedDate"].ToString()) : DateTime.MinValue
+        //        };
 
-        public IActionResult Index() {
-            return View();
-        }
+        //    }
+        //    return View(serialBase);
+        //}
 
-        public IActionResult Privacy() {
+        //public IActionResult Index()
+        //{
+        //    SerialBase_SP serialBaseSP = new SerialBase_SP("UR");
+        //    List<SerialBase> serialTable = serialBaseSP.GetSerialTable();
+        //    SerialBase serialBase = serialTable.FirstOrDefault(); // 取得第一筆資料
+        //    return View(serialBase);
+        //}
+
+        public IActionResult Privacy()
+        {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
     }
 }
