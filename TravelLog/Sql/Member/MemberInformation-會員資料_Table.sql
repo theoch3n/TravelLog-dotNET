5b39f316cf3bf25f4ec7863bc82a3360bfc4afda
david39128332 <david39128332@gmail.com>
2025-01-11 18:30:37 +0800

    [feat] 更新會員資料表 [fix] 修復錯誤

--- TravelLog/Sql/Member/MemberInformation-會員資料_Table.sql
+++ TravelLog/Sql/Member/MemberInformation-會員資料_Table.sql
@@ -34,3 +34,10 @@ ALTER TABLE [dbo].[MemberInformation] ADD  DEFAULT ((1)) FOR [MI_IsActive]
 GO
 
 
+
+-- 插入預設資料
+INSERT INTO [dbo].[MemberInformation]
+(MI_AccountName, MI_Email, MI_PasswordHash, MI_RegistrationDate, MI_IsActive, MIEmailConfirmationToken)
+VALUES 
+('Test', 'Test@gmail.com', '047f91c39524762a871344b62bc607418653f78a2e11fabb1dafb79968a99272', 
+ '2025-01-11 17:24:24.650', 1, NULL);
