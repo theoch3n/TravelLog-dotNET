80f35ac56a6d03aad84d4b1def4be946a9577974
JEC <wiseinfo31@gmail.com>
2025-01-22 09:42:16 +0800

    [feat] 1.新增TravelLogFront(前台專案) 2.新增TravelLogAPI(API專案)

---
+++ TravelLogFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelLogFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
