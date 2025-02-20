1f28d6ca326e84e296c16c98a70f76ba262ee177
David <David39128332@gmail.com>
2025-02-20 13:58:43 +0800

    [feat] 新增會員管理介面

---
+++ TravelLog/Controllers/UserController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelLog.Models;
+using TravelLog.ViewModels;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class UserController : Controller
+{
+    private readonly TravelLogContext _context;
+    public UserController(TravelLogContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /User/Index
+    public IActionResult Index()
+    {
+        // 取得所有 User 並 Include UserPds
+        var users = _context.Users.Include(u => u.UserPds).ToList();
+
+        // 轉換為 ViewModel
+        var viewModels = users.Select(user =>
+        {
+            var userPd = user.UserPds.FirstOrDefault(); // 只取第一筆
+            return new UserAccountViewModel
+            {
+                UserId = user.UserId,
+                UserName = user.UserName,
+                UserEmail = user.UserEmail,
+                UserPhone = user.UserPhone,
+                UserEnabled = user.UserEnabled,
+                UserCreateDate = user.UserCreateDate,
+                UserPdId = userPd?.UserPdId,
+                UserPdPasswordHash = userPd?.UserPdPasswordHash,
+                UserPdToken = userPd?.UserPdToken,
+                UserPdCreateDate = userPd?.UserPdCreateDate
+            };
+        }).ToList();
+
+        return View(viewModels);
+    }
+
+    // POST: /User/Edit
+    // 使用 AJAX 提交更新
+    [HttpPost]
+    public async Task<IActionResult> Edit([FromBody] UserAccountViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == model.UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.UserName = model.UserName;
+            user.UserEmail = model.UserEmail;
+            user.UserPhone = model.UserPhone;
+            user.UserEnabled = model.UserEnabled;
+            // UserCreateDate 通常不更新
+
+            var userPd = _context.UserPds.FirstOrDefault(p => p.UserPdId == model.UserPdId);
+            if (userPd != null)
+            {
+                userPd.UserPdPasswordHash = model.UserPdPasswordHash;
+                userPd.UserPdToken = model.UserPdToken;
+            }
+            else
+            {
+                userPd = new UserPd
+                {
+                    UserId = user.UserId,
+                    UserPdPasswordHash = model.UserPdPasswordHash,
+                    UserPdToken = model.UserPdToken,
+                    UserPdCreateDate = DateTime.Now
+                };
+                _context.UserPds.Add(userPd);
+            }
+
+            await _context.SaveChangesAsync();
+            return Json(new { success = true });
+        }
+        return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors) });
+    }
+}
---
+++ TravelLog/ViewModels/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TravelLog.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        // User 資料
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+        public bool UserEnabled { get; set; }
+        public DateTime UserCreateDate { get; set; }
+
+        // UserPd 資料 (此處使用 nullable 型別，以防該使用者沒有對應資料)
+        public int? UserPdId { get; set; }
+        public string UserPdPasswordHash { get; set; }
+        public string UserPdToken { get; set; }
+        public DateTime? UserPdCreateDate { get; set; }
+    }
+}
